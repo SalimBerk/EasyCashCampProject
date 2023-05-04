f133fa3cae62bf818c98f0e0725f4472d6b99cb6
SalimBerk <77536512+SalimBerk@users.noreply.github.com>
2023-05-04 22:06:42 +0300

    User Table have edited.
    04.05.2023

---
+++ EasyCashCampProject.DataAccessLayer/concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashCampProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashCampProject.DataAccessLayer.concrete
+{
+    public class Context:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;initial catalog=EasyCashDb;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder); 
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; } 
+        public DbSet<CustomerAccountProcess> CustomerAccountsProcess { get; set; }
+    }
+}
